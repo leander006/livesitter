517194bd28ab8b207cf5c8b1cfc41d8fb9167668
leander006 <leanderdsilva06@gmail.com>
2023-09-28 11:25:46 +0530

    Deploying ui

--- client/src/utils/helper.js
+++ client/src/utils/helper.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://leander-socail-media.onrender.com";
-export const BASE_URL = "http://localhost:3003";
+export const BASE_URL = "https://livesitter.onrender.com";
+// export const BASE_URL = "http://localhost:3003";
 export const rtspUrl = "https://rtsp.me/embed/EtKGh5BY/";
